0d3da70b0d29a8e07d4b729ca7a8894df4bfdb8b
Elias Ferreira <elias.eliasferreira@gmail.com>
2019-09-20 00:21:05 -0300

    first-commit

---
+++ src/components/Card/index.js
@@ -0,0 +1,61 @@
+import React, { useRef, useContext } from 'react';
+import { useDrag, useDrop } from 'react-dnd'; 
+import { Container, Label } from './styles';
+import BoardContext from '../Board/context';
+
+export default function Card({ data, index, listIndex }) {
+  const ref = useRef();
+  const { move } = useContext(BoardContext);
+
+  const [ {isDragging}, dragRef ] = useDrag({
+    item: { type: 'CARD', index, listIndex },
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
+    })
+  });
+
+  const [, dropRef] = useDrop({
+    accept: 'CARD',
+    hover(item, monitor) {
+      const draggedIndex = item.index;
+      const targetIndex = index;
+      const draggedListIndex = item.listIndex;
+      const targetListIndex = listIndex;
+
+      if(draggedIndex === targetIndex && draggedListIndex === targetListIndex) {
+        return;
+      }
+
+      const targetSize = ref.current.getBoundingClientRect();
+      const targetCenter = targetSize.height / 2;
+      // const targetCenter = (targetSize.bottom - targetSize.top) / 2;
+      const draggedOffset = monitor.getClientOffset();
+      const dreaggedTop = draggedOffset.y - targetSize.top;
+
+      if(draggedIndex < targetIndex && dreaggedTop < targetCenter ) {
+        return;
+      }
+
+      if(draggedIndex > targetIndex && dreaggedTop > targetCenter ) {
+        return;
+      }
+
+      move(draggedListIndex, targetListIndex, draggedIndex, targetIndex);
+      item.index = targetIndex;
+      item.listIndex = targetListIndex;
+    }
+  });
+
+  dragRef(dropRef(ref));
+
+  return (
+    <Container ref={ref} isDragging={isDragging}>
+      <header>
+        {data.labels.map(label => <Label key={label} color={label} />)}
+      </header>
+      <p>{data.content}</p>
+      
+      {data.user && (<img src={data.user} alt="" />)}
+    </Container>
+  );
+}
